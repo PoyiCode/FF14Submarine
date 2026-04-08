42cb51f6cad1095c16bd9219157c242c835871cc
PoyiCode <poyicode@gmail.com>
2026-04-08 20:24:26 +0800

    publish準備

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FF14Submarine/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
